5e7b3853fa36497cca60184a0e46dbc10bacd9ad
Andrew Serie <andrewserie@Bk.local>
2019-03-13 09:07:41 -0500

    still broken

--- project1/ViewCartPage.py
+++ project1/ViewCartPage.py
@@ -14,41 +14,29 @@ class ViewCartPage(Page):
         # Set breadcrumb frame
         self.label = tk.LabelFrame(
             self, text="Store > Cart", padx=10, pady=10)
-
-        self.label.grid_rowconfigure(0, weight=1)
-        self.label.grid_columnconfigure(0, weight=1)
-
-        yscrollbar = tk.Scrollbar(self.label)
-        yscrollbar.grid(sticky=tk.N+tk.S)
-
-        self.canvas = tk.Canvas(self.label, bd=0,yscrollcommand=yscrollbar.set)
-
-        self.canvas.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
-
-        yscrollbar.config(command=self.canvas.yview)
         self.label.pack(side="top", fill="both", expand=True)
-        self.canvas.config(scrollregion=self.canvas.bbox(tk.ALL))
-        # configure grid weights (resize)
-        self.canvas.grid_columnconfigure(0, weight=1)
-        self.canvas.grid_columnconfigure(1, weight=1)
-        self.canvas.grid_columnconfigure(2, weight=1)
-        self.canvas.grid_columnconfigure(3, weight=1)
-        self.canvas.grid_columnconfigure(4, weight=1)
-        self.canvas.grid_columnconfigure(5, weight=1)
-        self.canvas.grid_columnconfigure(6, weight=1)
-        self.canvas.grid_columnconfigure(7, weight=1)
-        self.canvas.grid_columnconfigure(8, weight=1)
-        self.canvas.grid_columnconfigure(9, weight=1)
-        self.canvas.grid_columnconfigure(10, weight=1)
-        self.canvas.grid_columnconfigure(11, weight=1)
+
+        # configure labelframe grid weights
+        self.label.grid_columnconfigure(0, weight=1)
+        self.label.grid_columnconfigure(1, weight=1)
+        self.label.grid_columnconfigure(2, weight=1)
+        self.label.grid_columnconfigure(3, weight=1)
+        self.label.grid_columnconfigure(4, weight=1)
+        self.label.grid_columnconfigure(5, weight=1)
+        self.label.grid_columnconfigure(6, weight=1)
+        self.label.grid_columnconfigure(7, weight=1)
+        self.label.grid_columnconfigure(8, weight=1)
+        self.label.grid_columnconfigure(9, weight=1)
+        self.label.grid_columnconfigure(10, weight=1)
+        self.label.grid_columnconfigure(11, weight=1)
 
         # Set nav buttons
-        tk.Button(self.canvas, text='Back to Store', width=15,
+        tk.Button(self.label, text='Back to Store', width=15,
                   command=self.hide).grid(row=0, column=0, columnspan=2, sticky="nw")
         cartDisabled = "normal"
         if (len(self.cart) < 1):
             cartDisabled = "disabled"
-        tk.Button(self.canvas, text='Clear Cart', width=15,
+        tk.Button(self.label, text='Clear Cart', width=15,
                   command=self.clearCart, state=cartDisabled).grid(row=0, column=10, columnspan=2, sticky="ne")
 
         # Set Main Image
@@ -56,15 +44,62 @@ class ViewCartPage(Page):
         imagesPath = Path(cwd, "images")
         imgPath = imagesPath / "bluebox.png"
         img = tk.PhotoImage(file=rf"{imgPath}")
-        mainImage = tk.Label(self.canvas, image=img)
+        mainImage = tk.Label(self.label, image=img)
         mainImage.image = img
         mainImage.grid(row=0, column=2, columnspan=8)
 
         # Show Discount offers
-        tk.Label(self.canvas, text='Orders of more than 50 boxes ship FREE', fg="dark red", font="Helvetica 20 bold").grid(
+        tk.Label(self.label, text='Orders of more than 50 boxes ship FREE', fg="dark red", font="Helvetica 20 bold").grid(
             row=1, column=2, columnspan=8)
-        tk.Label(self.canvas, text='Apply a 10% discount with a purchase of 150 boxes or more', fg="dark red", font="Helvetica 20 bold").grid(
+        tk.Label(self.label, text='Apply a 10% discount with a purchase of 150 boxes or more', fg="dark red", font="Helvetica 20 bold").grid(
             row=2, column=2, columnspan=8)
+
+        # canvas frame
+        canvFrame = tk.Frame(self.label)
+        canvFrame.grid_rowconfigure(0, weight=1)
+        canvFrame.grid_columnconfigure(0, weight=1)
+        canvFrame.grid(row=3, column=0, columnspan=12)
+
+        # Add canv to canvFrame
+        canvas = tk.Canvas(canvFrame, bg="yellow")
+        canvas.grid(row=0, column=0, columnspan=12)
+        canvas.grid_columnconfigure(0, weight=1)
+        canvas.grid_columnconfigure(1, weight=1)
+        canvas.grid_columnconfigure(2, weight=1)
+        canvas.grid_columnconfigure(3, weight=1)
+        canvas.grid_columnconfigure(4, weight=1)
+        canvas.grid_columnconfigure(5, weight=1)
+        canvas.grid_columnconfigure(6, weight=1)
+        canvas.grid_columnconfigure(7, weight=1)
+        canvas.grid_columnconfigure(8, weight=1)
+        canvas.grid_columnconfigure(9, weight=1)
+        canvas.grid_columnconfigure(10, weight=1)
+        canvas.grid_columnconfigure(11, weight=1)
+
+        # Add scrollbar y to canv
+        yscrollbar = tk.Scrollbar(
+            canvFrame, orient="vertical", command=canvas.yview)
+        yscrollbar.grid(row=0, column=1, sticky='ns')
+        canvas.configure(yscrollcommand=yscrollbar.set)
+        canvas.config(scrollregion=canvas.bbox("all"))
+
+        # add frame inside canvas
+        self.contFrame = tk.Frame(canvas, bg="blue")
+        canvas.create_window((0, 0), window=self.contFrame, anchor="nw")
+
+        # configure grid weights (resize)
+        self.contFrame.grid_columnconfigure(0, weight=1)
+        self.contFrame.grid_columnconfigure(1, weight=1)
+        self.contFrame.grid_columnconfigure(2, weight=1)
+        self.contFrame.grid_columnconfigure(3, weight=1)
+        self.contFrame.grid_columnconfigure(4, weight=1)
+        self.contFrame.grid_columnconfigure(5, weight=1)
+        self.contFrame.grid_columnconfigure(6, weight=1)
+        self.contFrame.grid_columnconfigure(7, weight=1)
+        self.contFrame.grid_columnconfigure(8, weight=1)
+        self.contFrame.grid_columnconfigure(9, weight=1)
+        self.contFrame.grid_columnconfigure(10, weight=1)
+        self.contFrame.grid_columnconfigure(11, weight=1)
 
         # setup the cart
         self.setCart()
@@ -78,34 +113,34 @@ class ViewCartPage(Page):
             total = 0.0
             for box in self.cart:
                 # set product image
-                boxItem = tk.Label(self.canvas, image=box.imageSm)
+                boxItem = tk.Label(self.contFrame, image=box.imageSm)
                 boxItem.image = box.imageSm
                 boxItem.grid(column=0, columnspan=2,
                              row=currentRow, rowspan=4)
                 self.cartItems.append(boxItem)
 
                 # Set Product Name
-                name = tk.Label(self.canvas, text=box.name,
+                name = tk.Label(self.contFrame, text=box.name,
                                 font="Helvetica 15 bold")
                 name.grid(row=currentRow, column=2, columnspan=3, sticky="w")
                 self.cartItems.append(name)
 
                 # Set Product price
-                price = tk.Label(self.canvas, text="Price: $"+"{:.2f}".format(box.price)+"/ea",
+                price = tk.Label(self.contFrame, text="Price: $"+"{:.2f}".format(box.price)+"/ea",
                                  font="Helvetica 15")
                 price.grid(row=currentRow+1, column=2,
                            columnspan=3, sticky="w")
                 self.cartItems.append(price)
 
                 # show addons
-                addons = tk.Label(self.canvas, text=self.getAddOns(
+                addons = tk.Label(self.contFrame, text=self.getAddOns(
                     box))
                 addons.grid(column=2, row=currentRow +
                             2, columnspan=5, sticky="w")
                 self.cartItems.append(addons)
 
                 # show quantity of boxes
-                quant = tk.Label(self.canvas, text="Quantity: " + str(box.quantity)
+                quant = tk.Label(self.contFrame, text="Quantity: " + str(box.quantity)
                                  )
                 quant.grid(column=9, row=currentRow, sticky="e")
                 total += self.getBoxTotal(box)
@@ -113,33 +148,33 @@ class ViewCartPage(Page):
 
                 # Remove item button
                 removeBtn = tk.Button(
-                    self.canvas, text='Remove', command=lambda: self.removeItemFromCart(box))
+                    self.contFrame, text='Remove', command=lambda: self.removeItemFromCart(box))
                 removeBtn.grid(row=currentRow+1, column=9, sticky="e")
                 self.cartItems.append(removeBtn)
 
                 # seperator
-                sep = ttk.Separator(self.canvas)
+                sep = ttk.Separator(self.contFrame)
                 sep.grid(row=currentRow+4, column=0,
                          columnspan=10, sticky="ew")
                 self.cartItems.append(sep)
                 currentRow += 5
 
             # display total
-            totalLab = tk.Label(self.canvas, text="Total: $" +
+            totalLab = tk.Label(self.contFrame, text="Total: $" +
                                 "{:.2f}".format(total))
             totalLab.grid(column=9, row=currentRow, sticky="e")
             self.cartItems.append(totalLab)
 
             # checkout button
             checkoutBtn = tk.Button(
-                self.canvas, text='Checkout', font="Helvetica 18", command=self.ViewCheckoutPageNav)
+                self.contFrame, text='Checkout', font="Helvetica 18", command=self.ViewCheckoutPageNav)
             checkoutBtn.grid(row=currentRow+1, column=9, sticky="e")
             self.cartItems.append(checkoutBtn)
         else:
             # display empty cart message
-            tk.Label(self.canvas, text='Your cart is empty!', font="Helvetica 25 bold").grid(
+            tk.Label(self.contFrame, text='Your cart is empty!', font="Helvetica 25 bold").grid(
                 row=3, column=2, columnspan=8, pady=(30, 0))
-            tk.Button(self.canvas, text='Back to Store', width=15,
+            tk.Button(self.contFrame, text='Back to Store', width=15,
                       command=self.hide).grid(row=4, column=5, columnspan=2)
 
     def removeItemFromCart(self, box):
